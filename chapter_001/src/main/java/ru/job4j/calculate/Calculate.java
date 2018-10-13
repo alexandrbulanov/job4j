4a47bdf109ec0e58636c39f2aa9f2bd7bed2630a
Alexandr Bulanov <alexbulanov001@gmail.com>
2018-10-13 17:16:59 +0300

    1.3. JavaDoc. Code convention #4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+* Class Класс для вычисления арифметических операций + - * / ^.
+*@author Alexandr Bulanov
+*@since 13.10.2018
+*@version 0.1
+*/
 public class Calculate {
+	
+	/**
+	* Конструктор, вывода строки в консоль.
+	*@param ard - arg.
+	*/
 	public static void main (String[] args){
 		System.out.print ("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+*@author Alexandr Bulanov
+*@since 13.10.2018
+*@version 0.1
+*/
+package ru.job4j.calculate;
