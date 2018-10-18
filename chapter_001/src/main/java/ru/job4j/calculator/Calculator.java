9c58624f89886d778725c120014682ba4c9e247c
Alexandr Bulanov <alexbulanov001@gmail.com>
2018-10-18 20:20:44 +0300

    3.1. Элементарный калькуляторV3

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,26 +1,60 @@
 package ru.job4j.calculator;
 
+/**
+*Class Calculator - класс для вычисления арифметических операций + - * /.
+*@author Alexandr Bulanov
+*@since 18.10.2018
+*@version 0.1
+*@param result - поле для хранения типов данных. 
+*/
 public class Calculator {
     private double result;
 
+	/**
+	*Сложение.
+	*@param first - первый аргумент.
+	*@param second - второй аргумент.
+	*@param result - результат.
+	*/	
     public void add(double first, double second) {
         this.result = first + second;
     }
 
+	/**
+	*Вычитание.
+	*@param first - первый аргумент.
+	*@param second - второй аргумент.
+	*@param result - результат.
+	*/	
     public void sub(double first, double second) {
-        this.result = first + second;
+        this.result = first - second;
     }
 
+	/**
+	*Умножение.
+	*@param first - первый аргумент.
+	*@param second - второй аргумент.
+	*@param result - результат.
+	*/
     public void mult(double first, double second) {
-        this.result = first + second;
+        this.result = first * second;
     }
 
+	/**
+	*Деление.
+	*@param first - первый аргумент.
+	*@param second - второй аргумент.
+	*@param result - результат.
+	*/
     public void div(double first, double second) {
-        this.result = first + second;
+        this.result = first / second;
     }
 
+	/**
+	*Возвращение значения поля result.	
+	*@param result - результат.
+	*/
     public double getResult() {
         return this.result;
     }
 }
-
--- chapter_001/src/main/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/main/java/ru/job4j/calculator/CalculatorTest.java
@@ -5,7 +5,24 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+*Class CalculatorTest - класс для тестирования арифметических операций + - * /.
+*@author Alexandr Bulanov
+*@since 18.10.2018
+*@version 0.1
+*@param result - поле для хранения типов данных. 
+*/
 public class CalculatorTest {
+	
+	/**
+	*Тест сложения.
+	*assertThat - это статический метод из класса org.junit.Assert.assertThat;
+	*Этот метод принимает две переменных. result и Matcher.
+	*Matcher - в данном примере - это методы статический метод из класса import static org.hamcrest.core.Is.is;
+	*Он определяет как сравнивать две переменных result и except.
+	*В этом пример он сравнивает значения. то есть
+	*result == except - если объекты равны. то код выполняется успешно. если выражения не равны. то программа падает с ошибкой.
+	*/	
     @Test
     public void whenAddOnePlusOneThenTwo() {
         Calculator calc = new Calculator();
@@ -14,6 +31,10 @@ public class CalculatorTest {
         double expected = 2D;
         assertThat(result, is(expected));
     }
+	
+	/**
+	*Тест вычитания.
+	*/	
     @Test
     public void whenSubOnePlusOneThenTwo() {
         Calculator calc = new Calculator();
@@ -22,6 +43,10 @@ public class CalculatorTest {
         double expected = 2D;
         assertThat(result, is(expected));
     }
+	
+	/**
+	*Тест умножения.
+	*/	
     @Test
     public void whenMultOnePlusOneThenTwo() {
         Calculator calc = new Calculator();
@@ -30,6 +55,10 @@ public class CalculatorTest {
         double expected = 2D;
         assertThat(result, is(expected));
     }
+		
+	/**
+	*Тест деления.
+	*/	
     @Test
     public void whenDivOnePlusOneThenTwo() {
         Calculator calc = new Calculator();
@@ -39,6 +68,3 @@ public class CalculatorTest {
         assertThat(result, is(expected));
     }
 }
-
-
-
