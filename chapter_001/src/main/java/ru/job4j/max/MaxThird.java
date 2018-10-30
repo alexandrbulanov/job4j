4a0760217af80e24f83cd6eee318a0887697f76e
Alexandr Bulanov <alexbulanov001@gmail.com>
2018-10-30 22:13:17 +0300

    4.2. Максимум из двух чисел V4

---
+++ chapter_001/src/main/java/ru/job4j/max/MaxThird.java
@@ -0,0 +1,20 @@
+package ru.job4j.max;
+
+/**
+ * Максимум из двух чисел.
+ *@author Alexandr Bulanov
+ *@since 28.10.2018
+ *@version 0.1
+ */
+
+public class MaxThird {
+    /**
+     *@param first - первое число.
+     *@param second - второе число.
+     *@param thid - второе число.
+     *@return Вывод максимального числа.
+     */
+    public int max (int first, int second, int thid){
+        return max(first, second, thid);
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/max/MaxThirdTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ *@author Alexandr Bulanov
+ *@since 28.10.2018
+ *@version 0.1
+ */
+
+public class MaxThirdTest {
+    /**
+     * Максимум из трех чисел.
+     */
+    @Test
+    public void whenFirstLessSecondLessThid() {
+        Max maxim = new Max();
+        int result = maxim.max(2, 4, 3);
+        assertThat(result, is(4));
+    }
+}
