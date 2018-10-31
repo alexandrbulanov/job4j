4455e6b3251c1677d63516d2d786fab3f9e04d7e
Alexandr Bulanov <alexbulanov001@gmail.com>
2018-10-31 20:43:03 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,28 @@
+package ru.job4j.loop;
+
+/**
+ *Class Counter - Подсчет суммы чётных чисел в диапазоне.
+ *@author Alexandr Bulanov
+ *@since 31.10.2018
+ *@version 0.1
+ */
+
+public class Counter {
+    /**
+     *Сложение.
+     *@param start - первый аргумент.
+     *@param finish - последний аргумент.
+     *@param sum подсчет суммы
+     *@param result - результат.
+     */
+    public int add(int start, int finish) {
+        int i, sum = 0;
+        for (i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+            }
+        }
+        return sum;
+
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ *@author Alexandr Bulanov
+ *@since 31.10.2018
+ *@version 0.1
+ */
+
+public class CounterTest {
+    /**
+     *Сложение.
+     */
+    @Test
+    public void add(int start, int finish) {
+        Counter сount = new Counter();
+        int result = count.add(1, 10);
+        assertThat(result, is(30));
+    }
+}
