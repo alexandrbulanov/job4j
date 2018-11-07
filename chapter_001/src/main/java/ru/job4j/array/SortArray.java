86035779069afbeeaa87abf1d9e3d1cf834bbade
Alexandr Bulanov <alexbulanov001@gmail.com>
2018-11-07 21:38:08 +0300

    Проверить массив, что он отсортирован.

---
+++ chapter_001/src/main/java/ru/job4j/array/SortArray.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+
+/**
+ *@author Alexandr Bulanov
+ *@since 07.11.2018
+ *@version 0.1
+ */
+
+import java.util.*;
+
+/**
+ * data: целочисленный массив
+ * n: количество элементов в массиве
+ */
+
+public class SortArray {
+    public static boolean masSorted(int[] data, int n) {
+        // Нулевое значение или менее двух элементов - сортируется.
+        if (data == null || n < 2) {
+            return true;
+        } else if (data[n - 2] > data[n - 1]) {
+            // Если элемент до (n-2) больше (n-1)
+            return false;
+        }
+        // recurse.
+        return masSorted(data, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int [] data = {2,3,5};
+        System.out.println(Arrays.toString(data) + (masSorted(data, data.length) ? " Отсортирован" : " НЕ отсортирован"));
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/SortArrayTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SortArrayTest {
+
+    @Test
+    public void masSorted() {
+        /*SortArray sortArray = new SortArray();
+        int [] data = {2,6,5};
+        assertThat();*/
+    }
+}
